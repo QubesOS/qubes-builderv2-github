QubesOS/qubes-builderv2-github

commit 513da249a916692347a1f0f8b7ccf4380c67db27

Fix template build skip condition
Template's "release status" have different concept of a version - it
reports for the latest built version, as there is no real tag associated
with template build. So, when doing new build, do not check if the
template is "not released" because that's still about old version, not
the one that is going to be built. Instead, tighten build timestamp
condition to refuse building the same version twice.

github-action.py
@@ -650,7 +650,7 @@ class AutoActionTemplate(BaseAutoAction):
                 raise AutoActionError(
                     f"Failed to read or parse timestamp: {str(exc)}"
                 ) from exc
-            if template_timestamp < timestamp_existing:
+            if template_timestamp <= timestamp_existing:
                 log.info(
                     f"Newer template ({timestamp_existing.strftime('%Y%m%d%H%M')}) already built."
                 )
@@ -660,43 +660,39 @@ class AutoActionTemplate(BaseAutoAction):
             config=self.config, manager=self.manager, templates=self.templates
         )
 
-        if (
-            release_status.get(self.templates[0].name, {}).get("status", None)
-            == "not released"
-        ):
-            stage = "build"
-            try:
-                self.notify_build_status(
-                    "building",
-                )
+        stage = "build"
+        try:
+            self.notify_build_status(
+                "building",
+            )
 
-                build_log_file = self.make_with_log(
-                    self.run_stages,
-                    stages=["prep", "build"],
-                )
+            build_log_file = self.make_with_log(
+                self.run_stages,
+                stages=["prep", "build"],
+            )
 
-                stage = "upload"
-                self.make_with_log(
-                    self.run_stages,
-                    stages=["sign", "publish", "upload"],
-                )
+            stage = "upload"
+            self.make_with_log(
+                self.run_stages,
+                stages=["sign", "publish", "upload"],
+            )
 
-                self.notify_upload_status(build_log_file)
+            self.notify_upload_status(build_log_file)
 
-            except AutoActionError as autobuild_exc:
-                self.notify_build_status(
-                    "failed", stage=stage, log_file=autobuild_exc.log_file
-                )
-                pass
-            except TimeoutError as timeout_exc:
-                raise AutoActionTimeout("Timeout reached for build!") from timeout_exc
-            except Exception as exc:
-                self.notify_build_status(
-                    "failed",
-                    additional_info=f"Internal error: '{str(exc.__class__.__name__)}'",
-                )
-                log.error(str(exc))
-                pass
+        except AutoActionError as autobuild_exc:
+            self.notify_build_status(
+                "failed", stage=stage, log_file=autobuild_exc.log_file
+            )
+            pass
+        except TimeoutError as timeout_exc:
+            raise AutoActionTimeout("Timeout reached for build!") from timeout_exc
+        except Exception as exc:
+            self.notify_build_status(
+                "failed",
+                additional_info=f"Internal error: '{str(exc.__class__.__name__)}'",
+            )
+            log.error(str(exc))
+            pass
 
     def upload(self):
         timestamp_file = (

commit 349298a8665e72458c45681d4a8f1f2b8c1d4d27

Fix building updated template
Avoid calling _check_release_status_for_template() before building the
template, as it will load _old_ template timestamp to
QubesTemplate.timestamp property (via TemplatePlugin.get_template_tag()
-> TemplatePlugin.get_template_timestamp() calls) and that will make
TemplatePlugin.get_template_tag() not use the new value.
This made the new template be built with the old timestamp.

Fixes: 513da24 "Fix template build skip condition"

github-action.py
@@ -690,10 +690,6 @@ class AutoActionTemplate(BaseAutoAction):
                 )
                 return
 
-        release_status = _check_release_status_for_template(
-            config=self.config, manager=self.manager, templates=self.templates
-        )
-
         with timeout(self.timeout):
             stage = "build"
             try: